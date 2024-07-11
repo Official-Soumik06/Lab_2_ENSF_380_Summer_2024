735cc45d31c1834b4258b56c18efa08b031183ed
malikmahdivika <almalik.mahdivika@ucalgary.ca>
2024-07-10 22:42:39 -0600

    feeding information initial commit

--- src/yyc_pet_resort_application/FeedingInformation.java
+++ src/yyc_pet_resort_application/FeedingInformation.java
@@ -1,5 +1,29 @@
 package yyc_pet_resort_application;
 
 public class FeedingInformation {
-
+	private String foodName;
+	private String instructions;
+	
+	//Constructors
+	FeedingInformation(String foodName, String instructions) {
+		this.foodName = foodName;
+		this.instructions = instructions;
+	}
+	FeedingInformation() {}
+	
+	//setters
+	public void setFoodName(String name) {
+		this.foodName = name;
+	}
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+	
+	//getters
+	public String getFoodName() {
+		return this.foodName;
+	}
+	public String getInstructions() {
+		return this.instructions;
+	}
 }
