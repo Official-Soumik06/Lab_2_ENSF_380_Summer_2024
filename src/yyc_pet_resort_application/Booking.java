6edee7c441b5b5edbab2ef4cda41cb5a76efca33
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-11 19:18:15 -0600

    Added Implementation To Booking.java File

--- src/yyc_pet_resort_application/Booking.java
+++ src/yyc_pet_resort_application/Booking.java
@@ -1,5 +1,78 @@
 package yyc_pet_resort_application;
 
+// Public Class Booking:
+
 public class Booking {
 
+// Private Data Members: 
+	
+	private String bookingID;
+	private String startDate;
+	private String endDate;
+	private Pet pet;
+	private Client client;
+	private BillingPayment[] payments;
+	
+// Getter Public Member Functions/Methods: 
+	
+	public String getBookingID() {
+		return bookingID;
+	}
+	
+	public String getStartDate() {
+		return startDate;
+	}
+	
+	public String getEndDate() {
+		return endDate;
+	}
+	
+	public Pet getPet() {
+		return pet;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+	
+	public BillingPayment[] getPayments() {
+		return payments;
+	}
+	
+// Setter Member Functions/Methods: 
+	
+	public void setBookingID(String bookingID) {
+		this.bookingID = bookingID;
+	}
+	
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+	
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	public void setBillingPayments(BillingPayment[] payments) {
+		this.payments = payments;
+	}
+	
+// Other Public Functions/Methods
+	
+	public void addPayment(BillingPayment payment) {
+		
+	}
+	
+	public void removePayment(BillingPayment payment) {
+		
+	}
+	
 }
